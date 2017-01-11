ded39408538d6a83ba9085c8c7aa498466067a78
nylea105 <dew_95531@windowslive.com>
2017-01-11 20:53:03 +0700

    random enemy

--- js/code.js
+++ js/code.js
@@ -1,6 +1,8 @@
 
     var player;
     var enemy;
+    var count;
+    
 
         var mainState = {
         preload: function() {
@@ -48,48 +50,61 @@
 
             //cursors
             this.cursors = this.input.keyboard.createCursorKeys(); 
-
+            this.spacebar = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
             
                 /*var num = game.rnd.integerInRange(0, 1);
                 if(num == 0){
                     x = 0;
                 }else if(num==1){
                     x=800;
-                }*/
+                }
                 this.enemy = game.add.sprite(0,450,'enemy');
                 game.physics.arcade.enable(this.enemy);
                 this.enemy.body.bounce.y = 0.25;
                 this.enemy.body.gravity.y=980;
-                //if (this.x == 0) {
-                this.enemy.body.velocity.x= 200;
-                //}else if(this.x == 800){
-                //    this.enemy.body.gravity.x= -100;
-                //}
+                if (this.x == 0) {
+                this.enemy.body.velocity.x = 200;
+                }else if(this.x == 800){
+                    this.enemy.body.gravity.x= -100;
+                }
                 this.enemy.body.collideWorldBounds = true;
                 this.enemy.animations.add('right', [3, 4, 5], 10, true);
                 this.enemy.animations.add('left', [9, 10, 11], 10, true);
-                this.enemy.frame = 6;
+                this.enemy.frame = 6;*/
                 
             
-            
+                
 
-            
+                game.time.events.loop(count, spawnEnemy, this);
+                this.enemy = spawnEnemy();
         },
 
         update: function() {
-            
             game.physics.arcade.collide(this.player, this.myWorld);
             game.physics.arcade.collide(this.enemy, this.myWorld);
+            console.log(this.count);
+            //enemy animetion
+            if (this.num <= 4){
+                this.enemy.animations.play('right');
+                  
+            }else if (this.num >= 5){
+                this.enemy.animations.play('left');
+                
+            }
+
+            //enemy hit tower
             if (game.physics.arcade.collide(this.enemy, this.myTower)){
-                this.enemy.destroy();
+                this.enemy.kill();
                 this.hp -= 1;
                 this.hpText.text = 'HP : ' + this.hp;
             }
-            if (game.physics.arcade.collide(this.enemy, this.player)){
-                this.enemy.destroy();
+
+            //overlap
+            if (checkOverlap(this.player,this.enemy)&&this.spacebar.isDown){
+                this.enemy.kill();
             }
 
-            
+            //checkCursor
             this.player.body.velocity.x = 0;
             if (this.cursors.right.isDown) {
               this.player.body.velocity.x = 200;
@@ -102,16 +117,59 @@
               this.player.frame = 6;
             }
 
-            if (this.num == 0){
-                this.enemy.animations.play('right');      
-            }else if (this.num == 1){
-                this.enemy.animations.play('left');
-            }
         }
     };
 
-    function spawnEnemy(x){
-            
+    //spawnEnemy
+    function spawnEnemy(){
+        //random x
+        this.num = game.rnd.integerInRange(0, 10);
+        this.x;
+        count = count(200);
+        //console.log(this.num);
+        if(this.num <= 4){
+            this.x = 0;
+        }else if(this.num >= 5){
+            this.x=800;
+        } 
+        //console.log(this.x);
+
+        //crate enemy
+        this.enemy = game.add.sprite(this.x,450,'enemy');
+        game.physics.arcade.enable(this.enemy);
+        this.enemy.body.bounce.y = 0.25;
+        this.enemy.body.gravity.y=980;
+        
+        //check move
+        if (this.x == 0) {
+            this.enemy.body.velocity.x = 200;
+        }else if(this.x == 800){
+            this.enemy.body.velocity.x= -200;
+        }
+
+        //animetion and collideWorldBounds
+        this.enemy.body.collideWorldBounds = true;
+        this.enemy.animations.add('right', [3, 4, 5], 10, true);
+        this.enemy.animations.add('left', [9, 10, 11], 10, true);
+        this.enemy.frame = 6;    
+        return this.enemy;
+    }
+
+    function checkOverlap(spriteA, spriteB) {
+
+    var boundsA = spriteA.getBounds();
+    var boundsB = spriteB.getBounds();
+
+    return Phaser.Rectangle.intersects(boundsA, boundsB);
+
+}
+    function count(num){
+        var i = 0
+        if(i == 0){
+           count = 2000; 
+        }
+        count -= num;
+        return count;
     }
 
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game');
