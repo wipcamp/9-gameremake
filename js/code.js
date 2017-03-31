e66fdaa169690ccf8d1805c42d5c9234154137f9
nylea105 <dew_95531@windowslive.com>
2017-03-31 13:41:51 +0700

    new bg, bug shoot

--- js/code.js
+++ js/code.js
@@ -36,24 +36,35 @@
     var createEnemy;
     var timer;
     var attack;
+    var tower;
+    var hpbar;
+    var buttonZ;
+    var buttonX;
+    var bulletGroup = [];
+    var bulletX;
+    var shootTime = 200;
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         function preload() {
             game.load.image('ground', 'assets/FloorOnly.png');
+            game.load.image('clound', 'assets/CloudOnly.png');
+            game.load.spritesheet('tower', 'assets/Tower.png', 200, 400);
             game.load.spritesheet('player', 'assets/Captain.png', 50, 75);
             game.load.spritesheet('enemy', 'assets/ninja_m.png', 32, 36);
             game.load.spritesheet('buttonS', 'assets/start.png', 150, 80);
             game.load.spritesheet('buttonT', 'assets/Tutorial.png', 150, 80);
             game.load.spritesheet('buttonM', 'assets/main.png', 150, 80);
+            game.load.spritesheet('hpbar', 'assets/HP.png', 200, 40);
             game.load.image('button', 'assets/button.png');
             game.load.image('mainBg', 'assets/MainMenuBackGround.png');
-            game.load.image('gameBg', 'assets/BackgroundWithNoCloud.png');
+            game.load.image('gameBg', 'assets/Background.png');
             game.load.image('howTo', 'assets/How-to-play-v10.png');
             game.load.image('logo', 'assets/LogoGame.png');
-
+            game.load.image('bullet', 'assets/Bullet.png');
 
         }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         function create() {
+
             game.physics.startSystem(Phaser.Physics.ARCADE);
             //backgroupColor
             //game.stage.backgroundColor = '#6666FF';
@@ -68,20 +79,33 @@
             ground.body.immovable = true;
 
             //tower
-            myTower = game.add.group();
-            myTower.enableBody = true;
-            var tower = myTower.create(360,game.world.height -100,'ground');
-            tower.scale.setTo(0.2, 1);
-            tower.body.immovable = true;
-            hp = 10;
+            this.tower = game.add.sprite(300,230,'tower');
+            this.tower.scale.setTo(1, 1);
+            game.physics.arcade.enable(this.tower);
+            this.tower.animations.add('idle',[0],10,true);
+            this.tower.body.collideWorldBounds = false;
+            //console.log(tower);
+
+            hp = 20;
             hpText;
 
             //HP
+            /*
+            this.hpbar = game.add.sprite(16,16,'hpbar');
+            this.hpbar.scale.setTo(1, 1);
+            game.physics.arcade.enable(this.hpbar);
+            this.hpbar.body.collideWorldBounds = false;
+            this.hpbar.animations.add('5',[0,1,2,3,4,5,6,7,8],10,true);
+            this.hpbar.animations.add('4',[9,10,11,12,13,14,15,16,17],10,true);
+            this.hpbar.animations.add('3',[18,19,20,21,22,23,24,25,26],10,true);
+            this.hpbar.animations.add('2',[27,28,29,30,31,32,33,34,35],10,true);
+            this.hpbar.animations.add('1',[36,37,38,39,40,41,42,44],10,true);
+            */
             hpText = game.add.text(16, 16, 'HP : ' + hp, {
                 fontSize: '20px',
                 fill: '#ed3465'
               });
-
+            
             scoreText = game.add.text(16, 45, 'SCORE : ' + score, {
                 fontSize: '20px',
                 fill: '#ed3465'
@@ -90,10 +114,15 @@
             //cursors
             cursors = game.input.keyboard.createCursorKeys();
             spacebar = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+            buttonZ = this.input.keyboard.addKey(Phaser.Keyboard.Z)
+            buttonX = this.input.keyboard.addKey(Phaser.Keyboard.X)
 
             
             enemys = game.add.group();
             enemys.enableBody = true;
+            bullets = game.add.group();
+            bullets.enableBody = true;
+
 
             createPlayer(game);
 
@@ -104,12 +133,18 @@
             spawnEnemy();
             //console.log("mainGame");
 
-
+            createBullet(game,500);
 
     }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         function update() {
             //spawnEnemy();
+            if (shootTime < 200) {
+                shootTime++;
+            }
+            
+            this.tower.animations.play('idle');
+
             game.physics.arcade.collide(player,myWorld);
             player.body.velocity.x = 0;
             if (cursors.right.isDown) {
@@ -131,21 +166,58 @@
                 }
                 
             }
-            /*if (spacebar.isDown){
+
+            if (buttonZ.isDown){
                 if(cursorL){
-                    return player.animations.play('attackLeft');
+                    player.animations.stop('left');
+                    player.animations.play('attackLeft');
                 }else{
-                    return player.animations.play('attackRight');
+                    player.animations.stop('right');
+                    player.animations.play('attackRight');
                 }
             }
-            */
 
+            if (shootTime === 200) {
+                if (buttonX.isDown) {
+                    console.log('player X : '+player.x);
+                    spawnBullet(player.x);
+                    for (var i = 0; i < bulletGroup.length; i++) {
+                        if(bulletGroup[i].alive){
+                            if (cursorL) {
+                                bulletGroup[i].body.velocity.x = -200;
+                            }else{
+                                bulletGroup[i].body.velocity.x = 200;
+                            } 
+                            if(checkOverlap(enemyGroup[i],bulletGroup[i])){
+                                enemyGroup[i].kill();
+                                bulletGroup[i].kill();
+                                console.log("shoot overlap");
+                                score += 10;
+                                scoreText.text = 'SCORE : ' + score;
+                                
+                            }
+                            if (game.physics.arcade.collide(bulletGroup[i],myWorld)) {
+                                bulletGroup[i].kill();
+                            }
+
+                        }
+                    }
+                    shootTime = 0; 
+                }
+            }
 
             
+
+            
+            
+
+
+            /*
             if (cursors.up.isDown) {
                 hp -= 1;
                 hpText.text = 'HP : ' + hp;
             }
+            */
 
             
             for( var i =0; i < enemyGroup.length ; i++){
@@ -157,30 +229,42 @@
                 if(enemyGroup[i].alive){
                     if (enemyGroup[i].x == 0){
                         enemyGroup[i].animations.play('right');
-                        return enemyGroup[i].body.velocity.x = 200;
+                        enemyGroup[i].body.velocity.x = 200;
                     }else if (enemyGroup[i].x == 800) {
                         enemyGroup[i].animations.play('left');
-                        return enemyGroup[i].body.velocity.x = -200;
+                        enemyGroup[i].body.velocity.x = -200;
                     }
                     
-                    if(checkOverlap(enemyGroup[i],player)&&spacebar.isDown){
-                      console.log("overlap");
+                    if(checkOverlap(enemyGroup[i],player)&&buttonZ.isDown){
+                        console.log("punch overlap");
                         score += 10;
                         scoreText.text = 'SCORE : ' + score;
-                        return enemyGroup[i].kill();
-                    }else if(enemyGroup[i].overlap(myTower)){
+                        enemyGroup[i].kill();
+                    }
+                    if(enemyGroup[i].overlap(this.tower)){
                         hp -= 1;
                         hpText.text = 'HP : ' + hp;
-                        return enemyGroup[i].kill();
+                        /*if (hp === 16){
+                            this.hpbar.animations.play('5');
+                        } else if (hp === 12){
+                            this.hpbar.animations.play('4');
+                        }else if (hp === 8){
+                            this.hpbar.animations.play('3');
+                        }else if (hp === 4){
+                            this.hpbar.animations.play('2');
+                        }else if (hp === 0){
+                            this.hpbar.animations.play('1');
+                        }*/
+                        enemyGroup[i].kill();
                     }
-                    
                 }
-            }
+                            }
 
             newGame();
             
-            
         }
+    
+
 ////////////////////////////Player///////////////////////////////////////
 
         function createPlayer(game){
@@ -198,6 +282,18 @@
             player.animations.add('attackLeft', [42,43,44,45,46,47,48,49,50,51,52], 20, true);
             player.animations.add('attackRight', [53,54,55,56,57,58,59,60,61,62,63], 11, true);
 
+        }
+
+        function spawnBullet(positionX){
+            bulletGroup.push(new createBullet(game,positionX));
+            console.log('Bullet X : '+bulletGroup[0].x);
+        }
+
+        function createBullet(game,bulletX){
+            this.bullet = bullets.create(bulletX, 500, 'bullet');
+            this.bullet.scale.setTo(100, 100);
+            this.bullet.body.immovable = true;
+            return this.bullet;
         }
 
 
@@ -223,7 +319,7 @@
                 //crate enemy
                 this.game = game;
                 this.alive = true;
-                this.enemy = enemys.create(this.x, 450,'enemy');
+                this.enemy = enemys.create(this.x, 515,'enemy');
                 this.enemy.anchor.set(0.5);
                 game.physics.arcade.enable(this.enemy);
                 this.enemy.body.gravity.y = 980;
@@ -339,4 +435,5 @@
         function howToPlayUpdate(){
 
         }
-        
+
+ 
